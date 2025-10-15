d079b3d750b3d3c49028446e15e301beb912f73b
brian <brian_llh@hotmail.com>
2025-10-15 23:47:51 +0800

    added control code

--- robotics_arm_control/include/robotics_arm_control/arm_control.hpp
+++ robotics_arm_control/include/robotics_arm_control/arm_control.hpp
@@ -6,9 +6,12 @@
 #include <termios.h>    // termios, tcgetattr(), tcsetattr()
 #include <unistd.h>     // read(), write(), close()
 #include <cstring>      // memset()
+#include <boost/asio.hpp>
 
+#include "pluginlib/class_list_macros.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <hardware_interface/system_interface.hpp>
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
 #include <rclcpp_lifecycle/state.hpp>
 #include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp>
 
@@ -34,12 +37,13 @@ namespace robotics_arm
             virtual hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
         private:
-
-            int SerialPort = -1;
             std::string port_;
+            int baudrate;
             std::vector<double> position_commands_;
             std::vector<double> prev_position_commands_;
             std::vector<double> position_states_;
+            boost::asio::io_service io_service_;
+            boost::asio::serial_port serial_port_;
     };
 
 }
--- robotics_arm_control/src/arm_control.cpp
+++ robotics_arm_control/src/arm_control.cpp
@@ -3,55 +3,161 @@
 namespace robotics_arm
 {
 
-RoboticsArm::RoboticsArm()
+RoboticsArm::RoboticsArm() : baudrate(115200), serial_port_(io_service_)
 {
-
 }
 
 
 RoboticsArm::~RoboticsArm()
 {
-    if (SerialPort != -1)
+    if (serial_port_.is_open())
     {
-        close(SerialPort);
+        serial_port_.close();
     }
 }
 
 CallbackReturn RoboticsArm::on_init(const hardware_interface::HardwareInfo &hardware_info)
 {
+    CallbackReturn result = hardware_interface::SystemInterface::on_init(hardware_info);
+    
+    if (result != CallbackReturn::SUCCESS)
+    {
+        return result;
+    }
+
+    try
+    {
+        port_ = info_.hardware_parameters.at("port");
+        baudrate = std::stoi(info_.hardware_parameters.at("baudrate"));
+    }
+    catch (const std::out_of_range &e)
+    {
+        RCLCPP_FATAL(rclcpp::get_logger("ArduinobotInterface"), "No Serial Port provided! Aborting");
+        return CallbackReturn::FAILURE;
+    }
+    
+
+    position_commands_.reserve(info_.joints.size());
+    position_states_.reserve(info_.joints.size());
+    prev_position_commands_.reserve(info_.joints.size());
+
     return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface> RoboticsArm::export_state_interfaces()
 {
-    return {};
+    std::vector<hardware_interface::StateInterface> state_interfaces;
+
+    // Provide only a position Interafce
+    for (size_t i = 0; i < info_.joints.size(); i++)
+    {
+        state_interfaces.emplace_back(hardware_interface::StateInterface(
+            info_.joints[i].name, hardware_interface::HW_IF_POSITION, &position_states_[i]));
+    }
+
+    return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface> RoboticsArm::export_command_interfaces()
 {
-    return {};
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+    // Provide only a position Interafce
+    for (size_t i = 0; i < info_.joints.size(); i++)
+    {
+        command_interfaces.emplace_back(hardware_interface::CommandInterface(
+            info_.joints[i].name, hardware_interface::HW_IF_POSITION, &position_commands_[i]));
+    }
+
+    return command_interfaces;
 }
 
 //read from arduino
 hardware_interface::return_type RoboticsArm::read(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
+    //Open Loop, current setup no encoder feedback
+    position_states_ = position_commands_;
     return hardware_interface::return_type::OK;
 }
 
 //write to arduino
 hardware_interface::return_type RoboticsArm::write(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
-    return hardware_interface::return_type::OK;
+    if (position_commands_ == prev_position_commands_)
+    {
+        // Nothing changed, do not send any command
+        return hardware_interface::return_type::OK;
+    }
+    std::string msg;
+    int joint1 = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(joint1));
+    msg.append(",");
+
+    int joint2 = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(joint2));
+    msg.append(",");
+
+
+    int joint3 = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(joint3));
+    msg.append(",");
+
+    int joint4 = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(joint4));
+    msg.append(",");
+
+    int joint5 = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(joint5));
+    msg.append(",");
+
+    int gripper = static_cast<int>(((position_commands_.at(0) + (M_PI / 2)) * 180) / M_PI);
+    msg.append(std::to_string(gripper));
+
 }
 
 CallbackReturn RoboticsArm::on_activate(const rclcpp_lifecycle::State &previous_state)
 {
-    return hardware_interface::CallbackReturn::SUCCESS;
+    RCLCPP_INFO(rclcpp::get_logger("RoboticsArm"), "Starting robot hardware ...");
+    boost::system::error_code ec;
+    serial_port_.open(port_, ec);
+    
+    if (ec) {
+        RCLCPP_WARN(rclcpp::get_logger("RoboticsArm"), "Failed to connect to serial port ");
+        return hardware_interface::CallbackReturn::FAILURE;
+    } else {
+        serial_port_.set_option(boost::asio::serial_port_base::baud_rate(
+            static_cast<uint32_t>(baudrate)));
+        serial_port_.set_option(boost::asio::serial_port_base::parity(
+            boost::asio::serial_port_base::parity::none));
+        RCLCPP_INFO(rclcpp::get_logger("RoboticsArm"), "Successfully connected to serial port ");
+        return CallbackReturn::SUCCESS;
+    }
 }
 
 CallbackReturn RoboticsArm::on_deactivate(const rclcpp_lifecycle::State &previous_state)
 {
-    return hardware_interface::CallbackReturn::SUCCESS;
+    if (serial_port_.is_open()) 
+    {
+        try
+        {
+            serial_port_.close();
+            RCLCPP_INFO(rclcpp::get_logger("RoboticsArm"), "Successfully connected to serial port %s",
+                    port_.c_str());
+        }
+        catch(...)
+        {
+            RCLCPP_FATAL_STREAM(rclcpp::get_logger("RoboticsArm"),
+                          "Something went wrong while closing connection with port " << port_);
+
+        }
+    } 
+    else 
+    {
+        RCLCPP_WARN_STREAM(rclcpp::get_logger("RoboticsArm"),
+                          "Something went wrong while closing connection with port " << port_);
+    }
+    RCLCPP_INFO(rclcpp::get_logger("ArduinobotInterface"), "Hardware stopped");
+    return CallbackReturn::SUCCESS;
 }
 
 }// namespace robotics_arm
